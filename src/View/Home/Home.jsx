d6ece7648bb977bf6d077980c7b4aa0d0e13c660
Emilio Bonmatí <emilre68@gmail.com>
2023-11-25 21:08:54 +0100

    create ecommerce card

--- src/View/Home/Home.jsx
+++ src/View/Home/Home.jsx
@@ -1,9 +1,11 @@
 import EcLayout from "../../Components/EcLayout/EcLayout"
+import EcCard from "../../Components/EcCard/EcCard"
 
 function Home() {
     return(
         <EcLayout>
             <p>Home</p>
+            <EcCard />
         </EcLayout>
     )
 }
